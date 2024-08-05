1a6f3eb0e129e80fc960369bc11cbf756acea987
gladyreigh <tgladyreigh@gmail.com>
2024-08-05 05:18:10 +0400

    Add files via upload

---
+++ part 2/part 2.6-2.10/src/App.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import Filter from './Filter';
+import PersonForm from './PersonForm';
+import Persons from './Persons';
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [search, setSearch] = useState('');
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const handleAddPerson = (event) => {
+    event.preventDefault();
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+    setPersons(persons.concat({ name: newName, number: newNumber }));
+    setNewName('');
+    setNewNumber('');
+  };
+
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter search={search} handleSearchChange={handleSearchChange} />
+      <h3>Add a new</h3>
+      <PersonForm
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        handleAddPerson={handleAddPerson}
+      />
+      <h3>Numbers</h3>
+      <Persons persons={filteredPersons} />
+    </div>
+  );
+};
+
+export default App;
